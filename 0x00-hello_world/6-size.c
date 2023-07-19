fecf7ffcda9c04cea9b5499ac5db92146845d7dd
Victor Lagat <lagovic@github.com>
2023-07-19 11:21:17 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <std.io>
+/**
+ * main - A program that prints the size of various computer types
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
