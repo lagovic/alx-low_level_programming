f90679fdbab1eb091f512cac930c5ae327ae11e9
Victor Lagat <lagovic@github.com>
2023-07-19 11:05:59 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <std.io>
+/**
+ * main - A program that prints aline using the printf function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
