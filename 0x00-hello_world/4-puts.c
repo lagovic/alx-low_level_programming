4ad0634772e4818f89c57fa162913d1e690e5d66
Victor Lagat <lagovic@github.com>
2023-07-19 10:58:34 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <std.io>
+/**
+ * main - A program printing a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
