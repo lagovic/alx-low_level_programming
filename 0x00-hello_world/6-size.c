d14142192db54e79ac1660aea70a4867e95fa6b5
Victor Lagat <lagovic@github.com>
2023-08-13 15:31:59 -0700

    6-size.cCO

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,6 @@ printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of float: %f byte(s)\n", (float)sizeof(f));
+
 return (0);
 }
