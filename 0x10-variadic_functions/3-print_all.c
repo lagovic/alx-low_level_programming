c99dd43cb86772c7648d0b7d31cdac80ed34133a
Victor Lagat <lagovic@github.com>
2023-08-20 03:46:25 -0700

    variadic  3-print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,9 +22,9 @@ while (format[i])
 switch (format[i])
 {
 case 'c':
-printf("%s%c", separator, va_arg(args, int));
+printf("%s%d", separator, va_arg(args, int));
 break;
-case 'i:
+case 'i':
 printf("%s%d", separator, va_arg(args, int));
 break;
 case 'f':
@@ -33,11 +33,11 @@ break;
 case 's':
 str = va_arg(args, char *);
 if (!str)
-str = "(nil)"
+str = "(nil)";
 printf("%s%s", separator, str);
 break;
 default:
-i++
+i++;
 continue;
 }
 separator = ", ";
