f235b76c9ea2fefd1f8dc1bc2303ae934b6a277a
lagovic <lagovic@gmail.com>
2023-10-24 16:52:13 -0700

    sum them all variadic function 3

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,23 +10,23 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-
-unsigned int i;
-int sum = 0;
-va_list args;
-
-if (n == 0)
-{
-        return (0);
-}
-else
-{
-va_start(args, n);
-for (i = 0; i < n; i++)
-{
-sum += va_arg(args, int);
-}
-}
-va_end(args);
-return (sum);
+	unsigned int i;
+	int sum = 0;
+	va_list args;
+	
+	if (n == 0)
+	{
+		return (0);
+	}
+	
+	else
+	{
+		va_start(args, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(args, int);
+		}
+	}
+	va_end(args);
+	return (sum);
 }
